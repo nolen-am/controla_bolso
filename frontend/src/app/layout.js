1b819fb87c6c096a7aeccea7cae62916fb96515a
lnn-am <lenonalcant@hotmail.com>
2024-10-27 13:31:45 -0300

    first commit

---
+++ frontend/src/app/layout.js
@@ -0,0 +1,10 @@
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Controla Bolso</title>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ frontend/src/app/page.js
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Controla Bolso!</h1>
+      <p>O sistema feito na esperança de que eu consiga em conter na hora de efetivar compras de promoções totalmente desnecessárias na minha vida e que só server para consumir meu suado dinheirinho.</p>
+    </div>
+  );
+}
